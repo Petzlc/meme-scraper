28becce579622f8e4fca893b372e4a668c652ec4
Petzlc <chlodwig.petzl@hotmail.com>
2024-05-16 13:04:42 +0100

    Adding test.yml

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+import fs from 'node:fs';
+import https from 'node:https';
 import * as cheerio from 'cheerio';
-import fs from 'fs';
-import https from 'https';
 import fetch from 'node-fetch';
 
 const urlMeme = 'https://memegen-link-examples-upleveled.netlify.app/';
@@ -31,17 +31,12 @@ function fetchHtml(url) {
 
 // Function to parse HTML content and extract image URLs using cheerio
 function extractImageUrl(html) {
-  // Load the HTML content into a cheerio instance
   const $ = cheerio.load(html);
-  // Initialize an empty array to store image URLs
   const imgUrl = [];
   // Iterate over each <img> element in the HTML content
   $('img').each((index, element) => {
-    // Extract the value of the 'src' attribute for the current <img> element
     const srcUrl = $(element).attr('src');
-    // Check if the 'src' attribute value exists (is truthy)
     if (srcUrl) {
-      // If the 'src' attribute value exists, push it into the imgUrl array
       imgUrl.push(srcUrl);
     }
   });
@@ -50,17 +45,12 @@ function extractImageUrl(html) {
 
 // Function to download images using node-fetch and save them to the specified directory
 async function downloadImages(imageUrl) {
-  // This  is used for error handling. Code inside the try block is executed, and if any errors occur, they are caught in the catch block.
   try {
     // for loop iterates over each image URL in the imageUrls array
     for (let i = 0; i < Math.min(imageUrl.length, 10); i++) {
-      // This line makes an HTTP request to fetch the image data from the current URL using fetch
       const response = await fetch(imageUrl[i]);
-      // This line converts the response data into a buffer using the .buffer() method provided by the node-fetch library.
       const imageBuffer = await response.buffer();
-      // for the filename
       const filename = `${(i + 1).toString().padStart(2, '0')}.jpg`;
-      // full path for saving the image
       const imagePath = `${dirPath}${filename}`;
       fs.writeFileSync(imagePath, imageBuffer);
       console.log(`Downloaded ${filename}`);
@@ -71,33 +61,9 @@ async function downloadImages(imageUrl) {
   }
 }
 
-// Function to execute the scraping task
-/* (async () => {
-  try {
-    // Fetch HTML content from the website
-    const html = await fetchHtml(urlMeme);
-    // Extract image URLs from the HTML content
-    const imageUrl = extractImageUrl(html);
-    // Create the 'memes' directory if it doesn't exist
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
-
-      console.log(`Directory ${dirPath} created.`);
-    } else {
-      console.log(`Directory ${dirPath} already exists.`);
-    }
-    // Download images to the 'memes' directory
-    await downloadImages(imageUrl);
-  } catch (error) {
-    console.log('Error:', error);
-  }
-})();*/ // Gives a error message: Promises must be awaited, end with a call to .catch, or end with a call to .then with a rejection handler.eslint@typescript-eslint/no-floating-promises
-
 fetchHtml(urlMeme)
   .then((html) => {
-    // Extract image URLs from the HTML content
     const imageUrl = extractImageUrl(html);
-    // Create the 'memes' directory if it doesn't exist
     if (!fs.existsSync(dirPath)) {
       fs.mkdirSync(dirPath);
       console.log(`Directory ${dirPath} created.`);
